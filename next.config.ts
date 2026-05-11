4cbb26d8b41a60669beb7756c00da9e714f9df85
Pelcom Admin <pelcom@admin.local>
2026-05-11 10:37:32 -0300

    Fix: add mercadopago to serverExternalPackages to fix bundling error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  serverExternalPackages: [],
+  serverExternalPackages: ["mercadopago"],
   experimental: {
     serverActions: {
       allowedOrigins: ["virtuadoc.automatech.tech", "localhost:3000"]
